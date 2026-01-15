96c98058d0f2ef15066065fa1d252544493222d6
Jonathan Solares <jonathansolares1.js1@gmail.com>
2026-01-14 21:34:07 -0600

    [FIX] product no image

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -33,12 +33,13 @@ const ProductCard = ({
               e.target.nextSibling.style.display = 'flex';
             }}
           />
-        ) : null}
-        <div className="product-no-image">
-          <svg style={{ width: '48px', height: '48px', color: '#9ca3af' }} fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-          </svg>
-        </div>
+        ) : (
+          <div className="product-no-image">
+            <svg style={{ width: '48px', height: '48px', color: '#9ca3af' }} fill="currentColor" viewBox="0 0 20 20">
+              <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
+            </svg>
+          </div>
+        )}
       </div>
       
       <div className="product-info">
